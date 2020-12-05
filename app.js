0032b7716cb0e907962acf9215160e56c23eb44f
Tina <Tina@macusers-MBP-4.attlocal.net>
2020-12-04 18:03:37 -0600

    added styling and modal

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 $(() => {
 
+
+  const $openBtn = $("#openModal");
+
+  const $modal = $("#modal");
+
+  const $closeBtn = $("#close");
+
+  const openModal = () => {
+    $modal.css("display", "block");
+  }
+
+  $openBtn.on("click", openModal);
+
+  const closeModal = () => {
+    $modal.css("display", "none")
+  }
+
+  $closeBtn.on("click", closeModal);
+
   $('form').on('submit', (event) => {
     event.preventDefault();
+    openModal;
 
     const userInput = $('input[type="text"]').val();
 
@@ -11,17 +31,36 @@ $(() => {
   }).then(
     (data) => {
 
-      $('.definition').empty();
-      const $class = $('<p>').appendTo('.definition').html(data[0].fl)
+      $('#modal-textbox').empty();
+      const $hr = $('<hr>').css("background-color", "black").css("height", "1px").css("border", "none")
+      const $h1 = $('<h1>').text(`${userInput} :`).css("font-family", "sans-serif").appendTo('#modal-textbox').append($hr)
+      const $class = $('<p>').css("font-family", "sans-serif").appendTo('#modal-textbox').html(data[0].fl)
 
       const define = () => {
         for(let i = 0; i < 3; i++){
           console.log(data[i].shortdef);
 
-          const $p = $('<p>').appendTo('.definition').html("🔍 " + data[i].shortdef);
+          const $p = $('<p>')
+                  .css("line-height", "150%")
+                  .css("font-family", "sans-serif")
+                  .appendTo('#modal-textbox')
+                  .html("🔍  " + data[i].shortdef);
         }
       }
       define()
     })
+
+
+
+
+    // $.ajax({
+    //   url: `https://bing-image-search1.p.rapidapi.com/images/${userInput}`
+    //   // url: `https://api.tenor.com/v1/search?q=${userInput}&key=O3BV287UVV0M&limit=1`,
+    // }).then(
+    //   (data) => {
+    //     // const $gif = $('<img>').attr("src", data.results[0].url).appendTo('definition')
+    //     console.log(data);
+    //   }
+    // )
   })
 })
