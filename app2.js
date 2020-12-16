73b504e91e49330e65b85aa4b9ba506762930be6
Tina <Tina@macusers-MBP-4.attlocal.net>
2020-12-16 15:50:49 -0600

    meta tags

--- app2.js
+++ app2.js
@@ -1,20 +1,20 @@
-$(() => {
-
-  $('form').on('submit', (event) => {
-    event.preventDefault();
-    openModal;
-
-    const userInput = $('input[type="text"]').val();
-
-    var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=btSEwRIppgqvwv4PzLVVOyWyrQPQr1mS&limit=5`);
-
-      xhr.done(function(data) {
-
-          const $gif = $('<img>')
-                      .attr("src", data.data[0].url)
-                      .appendTo('#modal-textbox');
-                    });
-
-    }
-  )
-})
+// $(() => {
+//
+//   $('form').on('submit', (event) => {
+//     event.preventDefault();
+//     openModal;
+//
+//     const userInput = $('input[type="text"]').val();
+//
+//     var xhr = $.get(`https://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=btSEwRIppgqvwv4PzLVVOyWyrQPQr1mS&limit=5`);
+//
+//       xhr.done(function(data) {
+//
+//           const $gif = $('<img>')
+//                       .attr("src", data.data[0].url)
+//                       .appendTo('#modal-textbox');
+//                     });
+//
+//     }
+//   )
+// })
